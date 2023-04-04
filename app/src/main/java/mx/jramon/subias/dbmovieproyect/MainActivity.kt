ccf912fd48faf22dd365f810564e8c34b5e95894
JOSE RAMON SUBIAS MENDOZA <jose.subias@elektra.com.mx>
2023-04-03 18:44:00 -0600

    config authentication

--- app/src/main/java/mx/jramon/subias/dbmovieproyect/MainActivity.kt
+++ app/src/main/java/mx/jramon/subias/dbmovieproyect/MainActivity.kt
@@ -2,10 +2,20 @@ package mx.jramon.subias.dbmovieproyect
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.ktx.app
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FirebaseApp.initializeApp(this@MainActivity)
+
+        if(Firebase.auth.currentUser != null){
+
+        }
     }
 }
